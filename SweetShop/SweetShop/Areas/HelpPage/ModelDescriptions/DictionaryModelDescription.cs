c0495a8ad36eb235dfec3806b4e8af1c74670bf9
Ioan-GabrielCamen <g.camen@deltapi.nl>
2022-01-28 14:00:29 +0200

    Added start project .NET FRAMEWORK WEB API

---
+++ SweetShop/SweetShop/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SweetShop.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SweetShop/SweetShop/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SweetShop.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
